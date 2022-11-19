c1f7d94141d7be07a2847ad6ed3cd11b21314127
ahsanahmed321 <88315014+ahsanahmed456@users.noreply.github.com>
2022-11-20 04:49:59 +0500

    event queue added

--- order/src/app.service.ts
+++ order/src/app.service.ts
@@ -1,14 +1,22 @@
-import { Injectable } from '@nestjs/common';
-
-const orders = [
-  { orderId: 1, price: 5, quantity: 1 },
-  { orderId: 1, price: 5, quantity: 1 },
-  { orderId: 1, price: 5, quantity: 1 },
-];
+import { Injectable, Inject } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class AppService {
+  private readonly orders = [
+    { orderId: 1, price: 5, quantity: 1 },
+    { orderId: 1, price: 5, quantity: 1 },
+    { orderId: 1, price: 5, quantity: 1 },
+  ];
+
+  constructor(@Inject('EVENT_QUEUE') private eventQueue: ClientProxy) {}
+
   getOrders() {
-    return orders;
+    return this.orders;
+  }
+
+  createOrder(order) {
+    this.orders.push(order);
+    this.eventQueue.emit('order-created', order);
   }
 }
--- user/src/app.service.ts
+++ user/src/app.service.ts
@@ -1,14 +1,22 @@
-import { Injectable } from '@nestjs/common';
-
-const users = [
-  { name: 'Ahsan', email: 'ahsan@gmail.com', password: '!23' },
-  { name: 'Zaid', email: 'ahsan@gmail.com', password: '!23' },
-  { name: 'Taha', email: 'ahsan@gmail.com', password: '!23' },
-];
+import { Injectable, Inject } from '@nestjs/common';
+import { ClientProxy } from '@nestjs/microservices';
 
 @Injectable()
 export class AppService {
+  private readonly users = [
+    { name: 'Ahsan', email: 'ahsan@gmail.com', password: '123' },
+    { name: 'Zaid', email: 'ahsan@gmail.com', password: '123' },
+    { name: 'Taha', email: 'ahsan@gmail.com', password: '123' },
+  ];
+
+  constructor(@Inject('EVENT_QUEUE') private eventQueue: ClientProxy) {}
+
   getUsers() {
-    return users;
+    return this.users;
+  }
+
+  createUser(user) {
+    this.users.push(user);
+    this.eventQueue.emit('user-created', user);
   }
 }
